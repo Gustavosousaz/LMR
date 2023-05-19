f991d256076d73aedfb06a9f8897dbd9da77458e
Gustavosousaz <gbw2920@gmail.com>
2023-05-18 21:59:21 -0300

    18/05/23 aprendendo JS

---
+++ JavaScriptFormularioDados/script.js
@@ -0,0 +1,10 @@
+let Nome = document.getElementById("Nome").value;
+let Endereço = document.getElementById("Endereço").value;
+let Email = document.getElementById("Email").value;
+let ConfEmail = document.getElementById("ConfEmail").value;
+let Senha = document.getElementById("Senha").value;
+let ConfSenha = document.getElementById("ConfSenha").value;
+
+function Nome() {
+    //conferencia dos campos
+}
