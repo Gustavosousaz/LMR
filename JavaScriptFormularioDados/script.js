795334ca20364c83fa4282318f3f0852d058b5e7
Gustavosousaz <gbw2920@gmail.com>
2023-06-02 20:42:52 -0300

    02/06/23

--- JavaScriptFormularioDados/script.js
+++ JavaScriptFormularioDados/script.js
@@ -5,6 +5,28 @@ let ConfEmail = document.getElementById("ConfEmail").value;
 let Senha = document.getElementById("Senha").value;
 let ConfSenha = document.getElementById("ConfSenha").value;
 
-function Nome() {
-    //conferencia dos campos
+function Enviar(){
+    // conferência dos Campos
+    if(Nome==""){
+        alert("Campo Nome não Preenchido");
+    }else if(Endereco==""){
+        alert("Campo Endereço não Preenchido");
+    }else if(Email==""){
+        alert("Campo Email não Preenchido");
+    }else if(Senha==""){
+        alert("Campo Senha não Preenchido");
+    } else{
+        alert("Formulário enviado")
+    }
+}
+function verificarEmail(){
+    if(Email != ConfEmail){
+        alert("Campo Confirmar Email não Confere")
+    }
+}
+
+function verificarSenha(){
+    if(Senha != CSonfSenha){
+        alert("Campo Confirmar Senha não Confere")
+    }
 }
